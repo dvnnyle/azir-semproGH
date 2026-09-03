028614b4f24d2f49a8eb6da2b63ac4a33746fe3d
dvnnyle <godannyyy@gmail.com>
2026-09-03 10:23:14 +0200

    Edit> legg til view model i accoutn stuff

---
+++ Models/AccountViewModel.cs
@@ -0,0 +1,14 @@
+namespace azir_sempro.Models;
+
+public class AccountViewModel
+{
+    public string FirstName { get; set; } = "";
+
+    public string LastName { get; set; } = "";
+
+    public string Email { get; set; } = "";
+
+    public string PhoneNumber { get; set; } = "";
+
+    public string Password { get; set; } = "";
+}
