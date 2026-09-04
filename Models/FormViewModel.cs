297f0bb79a50b8029af45f9d7c222a9be0e41979
dvnnyle <godannyyy@gmail.com>
2026-09-04 07:07:08 +0200

    flersidet navigering for skjema: setup

--- Models/FormViewModel.cs
+++ Models/FormViewModel.cs
@@ -2,5 +2,9 @@ namespace azir_sempro.Models;
 
 public class FormViewModel
 {
-    public string Name { get; set; } = "";
+    public string Tittel { get; set; } = "";
+    public string Kategori { get; set; } = "";
+    public string Lokasjon { get; set; } = "";
+    public string PunkterJson { get; set; } = "";
+    public string Beskrivelse { get; set; } = "";
 }
